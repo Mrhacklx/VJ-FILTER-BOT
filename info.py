c3c6ea0b7846f7e38ce76cc1366081747b0b4ea8
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2024-10-19 18:39:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -106,7 +106,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False))
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'onepagelink.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '8b05406c954d2e76788d71308f74fdd20b4c4cbb')
 
