2dfabbe9e14ab556343c4e5ae50e1b42e744d729
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2025-03-07 17:12:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,7 +9,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '27382550'))
 API_HASH = environ.get('API_HASH', 'c428034cb811de290315501d8e3c82b5')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7230063187:AAFc_vHgyLZ-sg5ZZQx3ighxMzgPnr616ag")
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
@@ -55,9 +55,9 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://dgentertainment293:VgnPJCOdGV9s8HJl@cluster0.lvkp3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
-DATABASE_NAME = environ.get('DATABASE_NAME', "techvjclonefilterbot")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
+DATABASE_URI = environ.get('DATABASE_URI', "")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_NAME = environ.get('DATABASE_NAME', "Mrhacklx")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
